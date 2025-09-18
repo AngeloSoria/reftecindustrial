df5ded425cec3555de0793c87466d5ffbe39f657
Reftec IT <reftec.it@gmail.com>
2025-09-18 13:51:21 +0800

    feat: Add Inter font and update color variables in CSS; initialize Lucide icons in JavaScript

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import { createIcons, icons } from 'lucide';
+
+// Wait until DOM is ready
+document.addEventListener('DOMContentLoaded', () => {
+    createIcons({ icons });
+});
