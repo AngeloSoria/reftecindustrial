7d13d934eb5e05718b4962f552ebf8473c962bfa
Reftec IT <reftec.it@gmail.com>
2025-09-18 13:51:10 +0800

    feat: Add routes for projects, products, and about us pages

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,18 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+Route::get('/projects', function () {
+    return view('projects');
+})->name('projects');
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
+
+Route::get('/about_us', function () {
+    return view('about_us');
+})->name('about_us');
+
 Route::get('/sample', function () {
     return view('sample');
 });
