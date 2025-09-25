41882822c9fa33d2ef5b2f771207b44fd4687fd7
Reftec IT <reftec.it@gmail.com>
2025-09-25 17:27:29 +0800

    added component design for topbar public view.

--- resources/views/components/topbarcompanycontacts.blade.php
+++ resources/views/components/topbarcompanycontacts.blade.php
@@ -1,20 +1,29 @@
-<section class="bg-brand-primary border border-green-300 text-white w-full h-[52px] font-inter px-3 grid grid-cols-2 md:grid-cols-3">
-    <div class="flex gap-2 items-center justify-start">
-        <x-zondicon-location class="w-6 h-6" />
-        <h2>Brgy. Ugong, Valenzuela City, Philippines</h2>
+<section
+    class="bg-brand-primary text-white w-full min-h-[52px] font-inter px-2 md:px-4 py-1 
+    hidden md:grid grid-cols-2 md:grid-cols-3 gap-y-1 md:gap-y-0 overflow-hidden">
+    <div class="flex gap-2 items-center justify-center sm:justify-start">
+        <x-zondicon-location class="w-4 h-4" />
+        <h2 class="text-xs md:text-sm">Brgy. Ugong, Valenzuela City, Philippines</h2>
     </div>
-    <div class="flex gap-2 flex-row items-center justify-center">
-        <p>Call Us:</p>
-        <h2>+63 2 8706 3393</h2>
+    <div class="flex gap-2 flex-row items-center justify-center sm:justify-end md:justify-center">
+        <x-bi-telephone-fill class="md:hidden" />
+        <p class="text-sm hidden md:block">Call Us:</p>
+        <a href="tel:+63287063393" class="hover:underline">
+            <h2 class="text-xs md:text-xl font-medium">+63 2 8706 3393</h2>
+        </a>
     </div>
-    <div class="bg-red-500/50 flex gap-2 flex-col items-end justify-center">
-        <div class="flex gap-2">
-            <x-entypo-mail class="w-6 h-6" />
-            <h2>Brgy. Ugong, Valenzuela City, Philippines</h2>
+    <div class="flex gap-1 col-span-2 md:col-span-1 flex-col sm:flex-row md:flex-col items-center sm:items-end justify-between md:justify-center">
+        <div class="flex gap-2 items-end justify-center">
+            <x-entypo-mail class="w-4 h-4" />
+            <a href="mailto:reftecindustrialsupply@gmail.com" class="hover:underline">
+                <h2 class="text-xs md:text-sm">reftecindustrialsupply@gmail.com</h2>
+            </a>
         </div>
-        <div class="flex gap-2">
-            <x-entypo-mail class="w-6 h-6" />
-            <h2>Brgy. Ugong, Valenzuela City, Philippines</h2>
+        <div class="flex gap-2 items-end justify-center">
+            <x-entypo-mail class="w-4 h-4" />
+            <a href="mailto:reftec.indl@gmail.com" class="hover:underline">
+                <h2 class="text-xs md:text-sm">reftec.indl@gmail.com</h2>
+            </a>
         </div>
     </div>
 </section>
