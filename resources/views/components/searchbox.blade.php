d14c9f90ffdc56e39cdb40bacf7d55b6178b17cf
Reftec IT <reftec.it@gmail.com>
2025-09-25 17:22:08 +0800

    Search box input component

---
+++ resources/views/components/searchbox.blade.php
@@ -0,0 +1,7 @@
+<div class="flex items-center gap-1 text-white bg-brand-primary rounded-sm px-2 py-1">
+    {{-- icon --}}
+    <div class="p-1">
+        <x-zondicon-search class="w-4 h-4" />
+    </div>
+    <input type="text" {{ $attributes->merge(['class' => 'w-full bg-brand-primary/80 rounded-sm px-4 py-1 focus:outline-1 outline-white/30 outline-offset-2', 'placeholder' => 'Search...']) }}>
+</div>
