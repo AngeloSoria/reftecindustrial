348cc03703a24695c92602942e71bc12845eda3d
Reftec IT <reftec.it@gmail.com>
2025-09-26 17:03:42 +0800

    Init AlpineJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,18 @@ import './bootstrap';
 import { createIcons, icons } from 'lucide';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+
+Alpine.start()
 
 AOS.init({
-  once: true, // Animate only once
+    once: true, // Animate only once
 });
 
 // Wait until DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
-  createIcons({ icons });
+    createIcons({ icons });
 });
+
