a83f60dd3926a7c2ccba9000f3c51b6e348aeb77
Reftec IT <reftec.it@gmail.com>
2025-10-02 17:31:22 +0800

    Updated to username instead of email.

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,7 +17,7 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'email' => $this->email,
+            'username' => $this->username,
             'created_at' => $this->created_at->toDateTimeString(),
             'updated_at' => $this->updated_at->toDateTimeString(),
         ];
