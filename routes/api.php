a79b6dee55d366b909d222c4259f876b0d85b883
Reftec IT <reftec.it@gmail.com>
2025-10-08 16:28:29 +0800

    Cleanup and added auth middleware.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\UserController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::get('/users', [UserController::class, 'index']);
-//     Route::get('/users/{id}', [UserController::class, 'show']);
-// });
-
-
-Route::get('/users/{perPage?}/{page?}', [UserController::class, 'getAllUsers'])->name('api.users.getAllUsers');
-Route::get('/user/{id}', [UserController::class, 'getUser'])->name('api.users.getUser');
+Route::middleware("auth:sanctum")->group(function () {
+    Route::get('/users/{perPage?}/{page?}', [UserController::class, 'getAllUsers'])->name('api.users.getAllUsers');
+    Route::get('/users/{id}', [UserController::class, 'getUser'])->name('api.users.getUser');
+});
 Route::get('/test', [UserController::class, 'test'])->name('api.users.test');
