4a87e9fffee3d98be13c04f9f9223dcd1f6565b6
Reftec IT <reftec.it@gmail.com>
2025-10-08 17:17:47 +0800

    imported Chart.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import { createIcons, icons } from 'lucide';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Alpine from 'alpinejs'
+import Chart from 'chart.js/auto';
+
 
 window.Alpine = Alpine
 
