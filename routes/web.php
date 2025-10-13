ac2ae0e98031dfa959a23f9405d2491d4ff1253f
Reftec IT <reftec.it@gmail.com>
2025-10-13 09:11:47 +0800

    New routes and Auth'd routes.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('home');
@@ -18,13 +19,19 @@ Route::get('/about', function () {
     return view('aboutus');
 })->name('aboutus');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
+Route::get('/login', [LoginController::class, 'showForm'])->name('login');
+Route::post('/login', [LoginController::class, 'submit'])->name('login');
 
 Route::middleware('auth')->group(function () {
+    Route::post('/logout', [App\Http\Controllers\Auth\LogoutController::class, 'perform'])->name('logout');
+
     Route::get('/dashboard', function () {
         return view('auth.dashboard');
     })->name('dashboard');
+
+    Route::get('/profile', function () {
+        return view('auth.user_profile');
+    })->name('profile');
+
+
 });
