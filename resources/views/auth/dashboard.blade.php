d6f82ccdee1c9844309121dcb5d050712dbcdf88
Reftec IT <reftec.it@gmail.com>
2025-10-13 17:20:56 +0800

    Testing of modular counter widget.

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,8 +1,14 @@
 <x-layouts.auth.app viewName="Dashboard" class="p-2 flex gap-2">
     <section class="flex flex-col gap-2">
         <section class="flex items-center gap-2">
-            <x-auth.widget.counter icon="fluentui-person-16-o" iconColor="bg-brand-secondary-300"
-                label="Total Visits (Week)" />
+            <x-auth.widget.counter
+                counter="11"
+                change="11"
+                ratio="100"
+                ratioType="increase"
+                icon="fluentui-person-16-o"
+                iconColor="bg-brand-secondary-300"
+                label="Total Visits (Month)" />
             <x-auth.widget.counter icon="fluentui-vehicle-car-16-o" iconColor="bg-accent-orange-300"
                 label="Total Car Trips (Month)" />
             <x-auth.widget.counter icon="fluentui-camera-dome-16-o" iconColor="bg-accent-lightseagreen-100"
