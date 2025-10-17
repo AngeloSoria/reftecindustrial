539da4c54659b68d65c63e13ca62d9d63e4d013b
Reftec IT <reftec.it@gmail.com>
2025-10-17 17:16:10 +0800

    added prop variable for dynamic scroll detect y axis value.

--- resources/views/components/public/btn_backtotop.blade.php
+++ resources/views/components/public/btn_backtotop.blade.php
@@ -1,3 +1,7 @@
+@props([
+    'scrollDetectValue' => 400
+])
+
 <div class="fixed bottom-0 right-0 p-4 z-[9999] w-full flex justify-end">
     <button title="button back-to-top" id="backToTopBtn" class="cursor-pointer bg-accent-orange-300 text-black px-3 py-3 rounded-full shadow-lg hover:bg-yellow-400 transition duration-300 opacity-0 pointer-events-none transform translate-y-4">
         @svg('fluentui-arrow-up-12-o', 'w-5 h-5')
@@ -21,7 +25,7 @@
 
         // On scroll, toggle button visibility
         window.addEventListener('scroll', function () {
-            if (window.scrollY > 100) {
+            if (window.scrollY > @json($scrollDetectValue)) {
                 showButton();
             } else {
                 hideButton();
@@ -31,8 +35,6 @@
         // On button click, scroll to top smoothly
         backToTopBtn.addEventListener('click', function () {
             window.scrollTo({ top: 0, behavior: 'smooth' });
-            console.log(1331);
-
         });
     });
 </script>
