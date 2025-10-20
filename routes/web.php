3307352c8da37978ec7ab82af6d9ba6e831b6ac3
Reftec IT <reftec.it@gmail.com>
2025-10-20 17:21:05 +0800

    refactor: streamline route definitions and improve organization

--- routes/web.php
+++ routes/web.php
@@ -1,80 +1,74 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VisitorController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\UploadController;
-
+use App\Http\Controllers\Auth\{LoginController, LogoutController};
+use App\Http\Controllers\{VisitorController, UploadController};
 use App\Http\Controllers\Contents\GeneralController;
 
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Response;
+/*
+|--------------------------------------------------------------------------
+| Public Routes
+|--------------------------------------------------------------------------
+*/
 
+Route::view('/', 'home')->name('home');
+Route::view('/projects', 'projects')->name('projects');
+Route::view('/products', 'products')->name('products');
+Route::view('/about', 'aboutus')->name('aboutus');
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'showForm')->name('login');
+    Route::post('/login', 'submit')->name('login.submit');
+});
 
-Route::get('/projects', function () {
-    return view('projects');
-})->name('projects');
-
-Route::get('/products', function () {
-    return view('products');
-})->name('products');
-
-Route::get('/about', function () {
-    return view('aboutus');
-})->name('aboutus');
-
-Route::get('/login', [LoginController::class, 'showForm'])->name('login');
-Route::post('/login', [LoginController::class, 'submit'])->name('login');
-
-
-
+/*
+|--------------------------------------------------------------------------
+| Protected Routes (Authenticated Users)
+|--------------------------------------------------------------------------
+*/
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', function () {
-        return view('auth.user_profile');
-    })->name('profile');
-    Route::get('/dashboard', function () {
-        return view('auth.dashboard');
-    })->name('dashboard');
-    Route::get('/content', function () {
-        return view('auth.content');
-    })->name('content');
-    Route::get('/site_monitor', function () {
-        return view('auth.site_monitor');
-    })->name('site_monitor');
-    Route::get('/cartrack', function () {
-        return view('auth.cartrack');
-    })->name('cartrack');
-    Route::get('/users', function () {
-        return view('auth.users');
-    })->name('users');
-    Route::get('/logs', function () {
-        return view('auth.logs');
-    })->name('logs');
-    Route::get('/files', function () {
-        return view('auth.files');
-    })->name('files');
 
+    // Simple view-only pages
+    Route::view('/profile', 'auth.user_profile')->name('profile');
+    Route::view('/dashboard', 'auth.dashboard')->name('dashboard');
+    Route::view('/content', 'auth.content')->name('content');
+    Route::view('/site_monitor', 'auth.site_monitor')->name('site_monitor');
+    Route::view('/cartrack', 'auth.cartrack')->name('cartrack');
+    Route::view('/users', 'auth.users')->name('users');
+    Route::view('/logs', 'auth.logs')->name('logs');
+    Route::view('/files', 'auth.files')->name('files');
 
-    Route::post('/logout', [App\Http\Controllers\Auth\LogoutController::class, 'logout'])->name('logout');
+    // Controllers
+    Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
     Route::post('/upload', [UploadController::class, 'upload'])->name('upload');
 
-
-    Route::group(['prefix' => 'visitors'], function () {
-        Route::get('/total', [VisitorController::class, 'getTotalVisitors'])->name('visitors.total');
-        Route::get('/unique', [VisitorController::class, 'getUniqueVisitors'])->name('visitors.unique');
-        Route::get('/top-pages/{limit?}', [VisitorController::class, 'getTopVisitedPages'])->name('visitors.top-pages');
-        Route::get('/countries', [VisitorController::class, 'getCountries'])->name('visitors.countries');
-        Route::get('/os', [VisitorController::class, 'getOs'])->name('visitors.os');
-        Route::get('/devices', [VisitorController::class, 'getDevices'])->name('visitors.devices');
-        Route::get('/countries-this-month', [VisitorController::class, 'getVisitsByCountryThisMonth'])->name('visitors.countries-this-month');
-        Route::get('/widget-data', [VisitorController::class, 'getDataForWidgetCounter'])->name('visitors.widget-data');
+    /*
+    |--------------------------------------------------------------------------
+    | Visitor Analytics Routes
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('visitors')->name('visitors.')->controller(VisitorController::class)->group(function () {
+        Route::get('/total', 'getTotalVisitors')->name('total');
+        Route::get('/unique', 'getUniqueVisitors')->name('unique');
+        Route::get('/top-pages/{limit?}', 'getTopVisitedPages')->name('top-pages');
+        Route::get('/countries', 'getCountries')->name('countries');
+        Route::get('/os', 'getOs')->name('os');
+        Route::get('/devices', 'getDevices')->name('devices');
+        Route::get('/countries-this-month', 'getVisitsByCountryThisMonth')->name('countries-this-month');
+        Route::get('/widget-data', 'getDataForWidgetCounter')->name('widget-data');
     });
 
-    Route::group(['prefix' => 'content'], function () {
-        Route::post('update/section/hero', [GeneralController::class, 'setHeroSection'])->name('update.content.section.hero');
+    /*
+    |--------------------------------------------------------------------------
+    | Content Management Routes
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('content')->name('content.')->controller(GeneralController::class)->group(function () {
+        // POST
+        Route::post('section/hero', 'setHeroSection')->name('update.section.hero');
+
+        // GET
+        Route::get('section/hero', 'getHeroSection')->name('get.section.hero');
     });
+
 });
