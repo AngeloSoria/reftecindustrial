933b768251b1aefcebb294075ea3a9dae64045c1
Reftec IT <reftec.it@gmail.com>
2025-10-20 17:41:42 +0800

    feature: Fixed component automatically support any x-* Alpine attributes, without you needing to define each one (x_data, x_ref, x_click, etc.) as props.

--- resources/views/components/auth/content/general/history.blade.php
+++ resources/views/components/auth/content/general/history.blade.php
@@ -1,5 +1,5 @@
 <h2 class="text-xl font-bold mb-2">Our History</h2>
 
 <div class="container mx-auto">
-    <x-layouts.quill_editor id="history_context" x_ref="123" x_data x_click="alert(123)"/>
+    <x-layouts.quill_editor id="history_context" x-ref="123" x-data @click=""/>
 </div>
--- resources/views/components/layouts/quill_editor.blade.php
+++ resources/views/components/layouts/quill_editor.blade.php
@@ -1,14 +1,14 @@
-@props([
-    'x_ref' => null,
-    'x_data' => " ",
-    'x_click' => null,
-])
+@php
+    // Collect all Alpine-related attributes (x-*, @*, :)
+    $alpineAttributes = collect($attributes->getAttributes())
+        ->filter(function ($value, $key) {
+            return str_starts_with($key, 'x-')
+                || str_starts_with($key, '@')
+                || str_starts_with($key, ':');
+        })
+        ->mapWithKeys(fn($value, $key) => [$key => $value])
+        ->all(); // ✅ convert to plain array
+@endphp
 
-<div
-@if($x_ref) x-ref="{{ $x_ref }}" @endif
-@if($x_data) x-data="{{ $x_data }}" @endif
-@if($x_click) @click="{{ $x_click }}" @endif
-
-id="{{ $attributes->get('id') }}"
-class="quill-editor min-w-[300px] min-h-[100px]">
+<div id="{{ $attributes->get('id') }}" {{ $attributes->except(array_keys($alpineAttributes))->merge(['class' => 'quill-editor min-w-[300px] min-h-[100px]'])->merge($alpineAttributes) }}>
 </div>
