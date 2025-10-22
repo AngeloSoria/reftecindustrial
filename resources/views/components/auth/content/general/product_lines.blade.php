bbef92c73a2ca690c39b6093efc85fd22ba46b31
Reftec IT <reftec.it@gmail.com>
2025-10-22 17:18:33 +0800

    feat: added alpine click functionality

--- resources/views/components/auth/content/general/product_lines.blade.php
+++ resources/views/components/auth/content/general/product_lines.blade.php
@@ -1,2 +1,10 @@
 <h2 class="text-xl font-bold mb-2">Product Lines</h2>
 <p>Display your different product categories or featured products here.</p>
+
+<button
+@click="$dispatch('open-modal', {'id':'exampleModal'})"
+class="cursor-pointer px-4 py-2 rounded bg-accent-orange-300 hover:bg-accent-orange-400 transition-colors">
+Click Me!
+</button>
+
+<x-auth.modal.add_product_line title="Add new Product Line"/>
