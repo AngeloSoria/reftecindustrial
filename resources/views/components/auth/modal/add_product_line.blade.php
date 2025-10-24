b82b89735148a52bde842f2107fa959f67dd2b02
Reftec IT <reftec.it@gmail.com>
2025-10-24 17:19:27 +0800

    refactor: changes.

--- resources/views/components/auth/modal/add_product_line.blade.php
+++ resources/views/components/auth/modal/add_product_line.blade.php
@@ -8,7 +8,7 @@
 ])
 
 <div
-    class="fixed inset-0 z-[100] flex items-center justify-center bg-black/50"
+    class="fixed inset-0 z-[100] flex items-center justify-center bg-black/50 w-full h-screen p-4"
     x-data="{ open: @js($show) }"
     x-show="open"
     x-cloak
@@ -19,31 +19,42 @@
         x-transition
         x-show="open"
         @click.away="open = false"
-        class="bg-white rounded-md shadow-xl w-full max-w-md overflow-hidden"
+        class="bg-white rounded-md shadow-xl w-full max-w-4xl max-h-full overflow-x-hidden overflow-y-auto"
     >
         <!-- Topbar -->
-        <div class="flex items-center justify-between px-4 py-3 border-b">
-            <h2 class="text-lg font-semibold text-gray-800">{{ $title }}</h2>
+        <div class="flex items-center justify-between px-4 py-3 border-b border-accent-darkslategray-100">
+            <h2 class="text-lg font-medium text-gray-800">{{ $title }}</h2>
             <button
                 type="button"
                 @click="open = false"
-                class="text-gray-400 hover:text-gray-600"
+                title="Close"
+                class="text-gray-400 hover:text-gray-600 cursor-pointer"
             >
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none"
-                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M6 18L18 6M6 6l12 12" />
-                </svg>
+                @svg('zondicon-close', 'w-4 h-4')
             </button>
         </div>
 
         <!-- Modal Body -->
         <div class="p-4">
-            {{ $slot }}
+            <div class="flex flex-col gap-1">
+                <label for="input_productLineName" class="text-sm font-medium">Name</label>
+                <input
+                class="px-4 py-2 rounded border-2 border-gray-200 focus:border-brand-primary-950 focus:outline-none"
+                id="input_productLineName" name="product_line_name" type="text" placeholder="Enter product line name..." required
+                aria-required="true" />
+            </div>
+
+            <br/>
+
+            <div class="flex flex-col gap-1">
+                <label class="text-sm font-medium">Image</label>
+                {{-- TODO: FIXME: acceptFile value is not being rendered. --}}
+                <x-layouts.file_upload_drag acceptFile="image/*" />
+            </div>
         </div>
 
         <!-- Footer Buttons -->
-        <div class="flex justify-end gap-2 px-4 py-3 border-t bg-gray-50">
+        <div class="flex justify-end gap-2 px-4 py-3 border-t border-accent-darkslategray-100 bg-gray-50">
             <button
                 type="button"
                 @click="{{ $onCancel ? $onCancel : 'open = false' }}"
