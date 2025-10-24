3a3cef9c5a6af8f75ecb31ce2f550791f4e8d92a
Reftec IT <reftec.it@gmail.com>
2025-10-24 17:10:07 +0800

    feat: product lines controller.

---
+++ app/Models/Contents/GeneralProductLines.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Contents;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GeneralProductLines extends Model
+{
+    protected $table = "contents_general_product_lines";
+    protected $fillable = ['name', 'image_path'];
+}
