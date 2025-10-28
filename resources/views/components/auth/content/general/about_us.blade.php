7f35aaa693819ff7d8345aa07f89fa23a4d55891
Reftec IT <reftec.it@gmail.com>
2025-10-28 17:09:01 +0800

    feat: sortjs implementation (wip)

--- resources/views/components/auth/content/general/about_us.blade.php
+++ resources/views/components/auth/content/general/about_us.blade.php
@@ -1,6 +1,18 @@
 <h2 class="font-medium text-lg mb-2">About Us</h2>
-<p>Introduce your team, mission, and company values here.</p>
+<div class="">
+    <h2>Gallery</h2>
 
-<div class="container mx-auto p-4">
+    <ul id="sortable-container_about_gallery"
+        class="p-4 bg-accent-darkslategray-300 rounded gap-2 grid grid-cols-2 md:grid-cols-3">
+        @for ($i = 0; $i < 3; $i++)
+            <li
+                data-id="sortable_item_{{ $i }}"
+                title="Drag to change order."
+                class="relative p-4 bg-accent-darkslategray-100 border-12 border-white rounded-lg cursor-move shadow-sm min-w-[33%] min-h-[200px]">
 
+                <span class="absolute top-1 left-1 text-xs">Item {{ $i }}</span>
+                <img title="Click to view" src="{{ asset('images/kingspan.jpg') }}" class="w-full h-full aspect-video object-contain cursor-pointer" />
+            </li>
+        @endfor
+    </ul>
 </div>
