5a7d59a926154e596dea4c7b03dd7440b899ef82
Reftec IT <reftec.it@gmail.com>
2025-10-29 13:35:25 +0800

    refactor: modified column name.

--- app/Models/Contents/GeneralProductLines.php
+++ app/Models/Contents/GeneralProductLines.php
@@ -3,9 +3,11 @@
 namespace App\Models\Contents;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Contents\Upload;
 
 class GeneralProductLines extends Model
 {
     protected $table = "contents_general_product_lines";
-    protected $fillable = ['name', 'image_path', 'visibility'];
+    protected $fillable = ['name', 'upload_id', 'visibility'];
+
 }
