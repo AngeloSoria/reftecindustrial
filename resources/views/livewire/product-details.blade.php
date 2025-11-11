e93bf1904b546433c8722f45358258ece787bfb2
Reftec IT <reftec.it@gmail.com>
2025-11-11 13:52:54 +0800

    chore: Removed livewire components.

--- resources/views/livewire/product-details.blade.php
+++
@@ -1,8 +0,0 @@
-<div x-data >
-    @if ($product ?? false)
-        <h2 class="text-xl font-bold mb-2">{{ $product->name }}</h2>
-        <p class="text-gray-600">{{ $product->created_at }}</p>
-    @else
-        <p>Product not found.</p>
-    @endif
-</div>
