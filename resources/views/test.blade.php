b0f446ecc40a2ea2aaeaaa093302916962f7ac02
Reftec IT <reftec.it@gmail.com>
2025-11-12 16:11:20 +0800

    chore: test view

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,18 +2,11 @@
 
     <button
         x-data
-        x-on:click="$dispatch('openModal', ['product-details', { mode: 'get', id: 4 }]); console.log(123);"
+        x-on:click="$dispatch('openmodal', ['product-details', { mode: 'get', id: 4 }]); console.log(123);"
         class="px-4 py-2 bg-blue-500 text-white rounded-lg cursor-pointer hover:bg-blue-600">
         Add Product
     </button>
 
-
-
-    <livewire:modal/>
-
-
-    <livewire:sender/>
-    <livewire:receiver/>
-    <livewire:modal/>
+    <livewire:test-flux/>
 
 </x-layouts.auth.app>
