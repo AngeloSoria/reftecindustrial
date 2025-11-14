a9f99b7e6bd2758f1ee8ea1a2ef23362547a35a4
Reftec IT <reftec.it@gmail.com>
2025-11-14 16:13:22 +0800

    feat: Restore handle option for sortable gallery and add early return for unchanged indices

--- resources/js/Sortable/sortableAboutUsGallery.js
+++ resources/js/Sortable/sortableAboutUsGallery.js
@@ -6,14 +6,17 @@ export function initSortableAboutUsGallery() {
     if (el) {
         Sortable.create(el, {
             animation: 150,
-            // handle: ".drag-handle", // optional: use a handle icon or area
+            handle: ".drag-handle", // optional: use a handle icon or area
             onEnd: (evt) => {
+                if(evt.oldIndex === evt.newIndex) { return }
+
                 console.log("Old index:", evt.oldIndex);
                 console.log("New index:", evt.newIndex);
 
                 // Example: send new order to backend
                 const order = Array.from(el.children).map(li => li.dataset.id);
                 console.log("New order:", order);
+
 
                 // axios.post("/update-order", { order })
                 //     .then(res => console.log("Saved:", res.data))
