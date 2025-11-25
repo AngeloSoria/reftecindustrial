38f0111f6f8b0997c561c2043dd2af501304f314
Reftec IT <reftec.it@gmail.com>
2025-11-25 09:41:46 +0800

    `Refactor code to improve performance and functionality in various components and middleware`

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Visitor;
+use Exception;
 use MonishRoy\VisitorTracking\Helpers\Visitor as VisitorTrackingHelper;
 
 class VisitorController extends Controller
@@ -38,65 +39,99 @@ class VisitorController extends Controller
         return response()->json(['devices' => VisitorTrackingHelper::devices()]);
     }
 
-    public function getVisitsByCountryThisMonth()
+    public function getVisitsByCountryThisMonth($unique = true)
     {
-        $start = now()->startOfMonth();
-        $end = now()->endOfMonth();
+        try {
+            $start = now()->startOfMonth();
+            $end   = now()->endOfMonth();
 
-        $visits = Visitor::select('country')
-            ->whereBetween('visited_at', [$start, $end])
-            ->selectRaw('COUNT(*) as total')
-            ->groupBy('country')
-            ->orderByDesc('total')
-            ->limit(10)
-            ->get();
+            $countExpr = $unique
+                ? 'COUNT(DISTINCT ip) as total'
+                : 'COUNT(*) as total';
 
+            $visits = Visitor::select('country')
+                ->whereBetween('visited_at', [$start, $end])
+                ->selectRaw($countExpr)
+                ->groupBy('country')
+                ->orderByDesc('total')
+                ->limit(10)
+                ->get();
 
-        return response()->json($visits);
+            return response()->json([
+                'success' => true,
+                'data' => $visits
+            ]);
+        } catch (Exception $e) {
+            Logger()->error($e->getMessage());
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 
-    public function getTotalVisitsThisMonthValue()
+
+    public function getTotalVisitsThisMonthValue($unique = false)
     {
         $start = now()->startOfMonth();
         $end   = now()->endOfMonth();
 
-        return Visitor::whereBetween('visited_at', [$start, $end])->count();
+        $query = Visitor::whereBetween('visited_at', [$start, $end]);
+
+        return $unique ? $query->count('ip') : $query->count();
     }
 
-    public static function getTotalVisitsLastMonthValue()
+    public static function getTotalVisitsLastMonthValue($unique = false)
     {
-        return Visitor::whereBetween('visited_at', [now()->subMonth()->startOfMonth(), now()->subMonth()->endOfMonth()])->count();
+        $start = now()->subMonth()->startOfMonth();
+        $end = now()->subMonth()->endOfMonth();
+
+        $query = Visitor::whereBetween('visited_at', [$start, $end]);
+
+        return $unique ? $query->count('ip') : $query->count();
     }
 
     public function getDataForWidgetCounter()
     {
-        $visitsThisMonth = self::getTotalVisitsThisMonthValue();
-        $visitsFromLastMonth = self::getTotalVisitsLastMonthValue();
-        $visitsChangeFromLastMonth = $visitsThisMonth - $visitsFromLastMonth;
+        try {
+            $visitsThisMonth = self::getTotalVisitsThisMonthValue(true);
+            $visitsFromLastMonth = self::getTotalVisitsLastMonthValue(true);
+            $visitsChangeFromLastMonth = $visitsThisMonth - $visitsFromLastMonth;
 
-        if ($visitsFromLastMonth > 0) {
-            $visitChangeRatio = round(($visitsChangeFromLastMonth / $visitsFromLastMonth) * 100, 2);
-        } else {
-            // If last month = 0
-            $visitChangeRatio = $visitsThisMonth > 0 ? 100 : 0;
+            if ($visitsFromLastMonth > 0) {
+                $visitChangeRatio = round(($visitsChangeFromLastMonth / $visitsFromLastMonth) * 100, 2);
+            } else {
+                // If last month = 0
+                $visitChangeRatio = $visitsThisMonth > 0 ? 100 : 0;
+            }
+
+            // Determine ratio type
+            if ($visitsFromLastMonth === 0 && $visitsThisMonth === 0) {
+                $visitsRatioType = 'null'; // no data at all
+            } elseif ($visitsThisMonth > $visitsFromLastMonth) {
+                $visitsRatioType = 'increase';
+            } elseif ($visitsThisMonth < $visitsFromLastMonth) {
+                $visitsRatioType = 'decrease';
+            } else {
+                $visitsRatioType = 'neutral'; // same value
+            }
+
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'counter' => $visitsThisMonth,
+                    'change' => $visitsChangeFromLastMonth,
+                    'ratio' => $visitChangeRatio,
+                    'ratioType' => $visitsRatioType,
+                ],
+            ]);
+        } catch (Exception $e) {
+            Logger()->error($e->getMessage());
+
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
         }
-
-        // Determine ratio type
-        if ($visitsFromLastMonth === 0 && $visitsThisMonth === 0) {
-            $visitsRatioType = 'null'; // no data at all
-        } elseif ($visitsThisMonth > $visitsFromLastMonth) {
-            $visitsRatioType = 'increase';
-        } elseif ($visitsThisMonth < $visitsFromLastMonth) {
-            $visitsRatioType = 'decrease';
-        } else {
-            $visitsRatioType = 'neutral'; // same value
-        }
-
-        return response()->json([
-            'counter' => $visitsThisMonth,
-            'change' => $visitsChangeFromLastMonth,
-            'ratio' => $visitChangeRatio,
-            'ratioType' => $visitsRatioType,
-        ]);
     }
 }
