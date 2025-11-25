7786f4c1bf5952cf6835da1bb33d655140eee403
Reftec IT <reftec.it@gmail.com>
2025-11-25 09:43:54 +0800

    `Updated sortable gallery functionality with new features and bug fixes`

--- resources/js/Counters/totalVisits.js
+++ resources/js/Counters/totalVisits.js
@@ -5,25 +5,34 @@ export async function initVisitorsChart() {
 
     async function fetchAndRefreshValue() {
         try {
-            const response = await fetch('/visitors/widget-data');
-            const data = await response.json();
-
             const counterElement = document.querySelector('#widget_counter_total_visits .counter-value');
             const changeElement = document.querySelector('#widget_counter_total_visits .change-value');
             const ratioElement = document.querySelector('#widget_counter_total_visits .ratio-value');
+                        
+            const response = await fetch('/visitors/widget-data');
+            const data = await response.json();
+            console.log(data);
+            
+            // fetch fail
+            if(!data['success']) {
+                counterElement.textContent = "ERROR";
+                changeElement.textContent = "ERROR";
+                ratioElement.textContent = "ERROR";
+                return;
+            }
 
             if (counterElement) {
-                counterElement.textContent = data.counter.toLocaleString();
+                counterElement.textContent = data.data.counter.toLocaleString();
             }
             if (changeElement) {
-                const changeValue = data.change;
+                const changeValue = data.data.change;
                 changeElement.textContent = (changeValue > 0 ? '+' : '') + changeValue.toLocaleString() + ' difference';
                 changeElement.classList.toggle('hidden', changeValue == 0);
                 changeElement.classList.toggle('text-green-600', changeValue > 0);
                 changeElement.classList.toggle('text-red-600', changeValue < 0);
             }
             if (ratioElement) {
-                const ratioValue = data.ratio;
+                const ratioValue = data.data.ratio;
                 ratioElement.textContent = (ratioValue > 0 ? '+' : '') + ratioValue.toFixed(2) + '%';
                 ratioElement.classList.toggle('text-gray-500', ratioValue == 0);
                 ratioElement.classList.toggle('text-green-600', ratioValue > 0);
