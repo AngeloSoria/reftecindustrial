0176687f3ab87bead83b8840a69a9204bd16ba83
Reftec IT <reftec.it@gmail.com>
2025-12-01 17:28:49 +0800

    feat: renamed variable names and added a default value if no value passed from server.

--- resources/views/components/layouts/auth/content/general.blade.php
+++ resources/views/components/layouts/auth/content/general.blade.php
@@ -1,6 +1,11 @@
 <section class="font-inter flex item-start justify-start flex-wrap flex-col md:flex-row pb-2">
-
-    <div x-data="{ activeTab: '{{ session('content') ? session('content')['section'] : 'hero' }}' }" class="w-full flex flex-col md:flex-row gap-6">
+    @php
+        $activeSection = 'hero';
+        if(session('content') && !empty(session('content')['section'])) {
+            $activeSection = session('content')['section'];
+        }
+    @endphp
+    <div x-data="{ activeSection: '{{ $activeSection }}' }" class="w-full flex flex-col md:flex-row gap-6">
         {{-- Tabs links --}}
         <div class="md:min-w-[200px]">
             <div class="sticky top-14 md:px-8 md:py-6 md:border-r-2 border-gray-200/75">
@@ -9,20 +14,20 @@
                     [&>*]:hover:bg-accent-darkslategray-200/50 [&>*]:cursor-pointer [&>*]:px-3 [&>*]:py-1 [&>*]:text-sm
                     ">
 
-                    <li :class="activeTab === 'hero' ? 'text-accent-orange-300 border-b-3 border-accent-orange-300  md:border-none md:list-disc' : ''"
-                        @click="activeTab = 'hero'">
+                    <li :class="activeSection === 'hero' ? 'text-accent-orange-300 border-b-3 border-accent-orange-300  md:border-none md:list-disc' : ''"
+                        @click="activeSection = 'hero'">
                         Hero Section
                     </li>
-                    <li :class="activeTab === 'products' ? 'text-accent-orange-300 border-b-3 border-accent-orange-300  md:border-none md:list-disc' : ''"
-                        @click="activeTab = 'products'">
+                    <li :class="activeSection === 'products' ? 'text-accent-orange-300 border-b-3 border-accent-orange-300  md:border-none md:list-disc' : ''"
+                        @click="activeSection = 'products'">
                         Product Lines
                     </li>
-                    <li :class="activeTab === 'history' ? 'text-accent-orange-300 border-b-3 border-accent-orange-300 md:border-none md:list-disc' : ''"
-                        @click="activeTab = 'history'">
+                    <li :class="activeSection === 'history' ? 'text-accent-orange-300 border-b-3 border-accent-orange-300 md:border-none md:list-disc' : ''"
+                        @click="activeSection = 'history'">
                         History
                     </li>
-                    <li :class="activeTab === 'about' ? 'text-accent-orange-300 border-b-3 border-accent-orange-300 md:border-none md:list-disc' : ''"
-                        @click="activeTab = 'about'">
+                    <li :class="activeSection === 'about' ? 'text-accent-orange-300 border-b-3 border-accent-orange-300 md:border-none md:list-disc' : ''"
+                        @click="activeSection = 'about'">
                         About Us
                     </li>
                 </ul>
@@ -32,19 +37,19 @@
 
         {{-- Preview / Tab Content --}}
         <div class="grow min-h-[300px] p-4 w-full">
-            <div x-show="activeTab === 'hero'" x-transition>
+            <div x-show="activeSection === 'hero'" x-transition>
                 <x-auth.content.general.hero_section />
             </div>
 
-            <div x-show="activeTab === 'products'" x-transition>
+            <div x-show="activeSection === 'products'" x-transition>
                 <x-auth.content.general.product_lines />
             </div>
 
-            <div x-show="activeTab === 'history'" x-transition>
+            <div x-show="activeSection === 'history'" x-transition>
                 <x-auth.content.general.history />
             </div>
 
-            <div x-show="activeTab === 'about'" x-transition>
+            <div x-show="activeSection === 'about'" x-transition>
                 <x-auth.content.general.about_us />
             </div>
         </div>
@@ -52,4 +57,3 @@
 
 
 </section>
-<x-public.modal.image_preview escKeyToClose clickOutsideToClose />
