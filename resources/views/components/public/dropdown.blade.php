d4751b123ad2c6e5e90351d2a3fc0bbf3346fcc0
Reftec IT <reftec.it@gmail.com>
2025-12-15 17:35:18 +0800

    feat: dynamic attribute

--- resources/views/components/public/dropdown.blade.php
+++ resources/views/components/public/dropdown.blade.php
@@ -2,24 +2,23 @@
     'label' => null,
 ])
 
-<div class="relative {{ $attributes->get('class') }}">
-    @if ($label ?? false)
-        <p class="block text-sm font-medium text-gray-700 mb-1">{{ $label }}</p>
+<div {{ $attributes->except(['name', 'id', 'title', 'required', 'multiple', 'class'])->class('relative') }}>
+    @if ($label)
+        <p class="block text-sm font-medium text-gray-700 mb-1">
+            {{ $label }}
+        </p>
     @endif
 
     <div class="relative">
         <select
-
-            @if($id ?? false) id="{{ $id }}" @endif
-            @if($name ?? false) name="{{ $name }}" @endif
-            @if($title ?? false) title="{{ $title }}" @endif
-            @if($required ?? false) required @endif
-            @if($multiple ?? false) multiple @endif
-
-            class="w-full font-medium px-4 py-2 appearance-none rounded-sm bg-brand-primary-950 hover:bg-brand-primary-800 active:bg-brand-primary-800 text-white">
-
+            {{ $attributes->merge([
+                'class' => 'w-full font-medium px-4 py-2 appearance-none rounded-sm bg-brand-primary-950 hover:bg-brand-primary-800 active:bg-brand-primary-800 text-white'
+            ]) }}
+        >
             @if ($placeholder ?? false)
-                <option disabled selected class="text-gray-500">{{ $placeholder }}</option>
+                <option disabled selected class="text-gray-500">
+                    {{ $placeholder }}
+                </option>
             @endif
 
             {{ $slot }}
--- resources/views/components/public/searchbar.blade.php
+++ resources/views/components/public/searchbar.blade.php
@@ -4,15 +4,19 @@
     'id' => 'searchbar-' . uniqid(),
 ])
 
-<div class="{{ $attributes->get('class') }} flex items-center gap-1 text-white bg-brand-primary-950 rounded-sm px-2 py-1">
+<div {{ $attributes->except(['class', 'placeholder', 'name', 'id'])->class('flex items-center gap-1 text-white bg-brand-primary-950 rounded-sm px-2 py-1') }}>
     {{-- icon --}}
     <div class="p-1">
         @svg('fluentui-search-12', 'w-4 h-4')
     </div>
+
     <input
-        @if($placeholder) placeholder="{{ $placeholder }}" @endif
-        @if($name) name="{{ $name }}" @endif
-        @if($id) id="{{ $id }}" @endif
         type="text"
-        class="w-full bg-brand-primary-950/80 rounded-sm px-4 py-1 focus:outline-1 outline-white/30 outline-offset-2">
+        placeholder="{{ $placeholder }}"
+        name="{{ $name }}"
+        id="{{ $id }}"
+        {{ $attributes->merge([
+            'class' => 'w-full bg-brand-primary-950/80 rounded-sm px-4 py-1 focus:outline-1 outline-white/30 outline-offset-2'
+        ]) }}
+    >
 </div>
