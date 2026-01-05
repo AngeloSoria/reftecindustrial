f2e8c5c72fd5414782e83d02fda99f5d39a08b12
Reftec IT <reftec.it@gmail.com>
2026-01-05 17:07:18 +0800

    feat: refactored fake data for appropriate creation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,9 +25,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'username' => fake()->userName(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => 'Admin',
             'remember_token' => Str::random(10),
         ];
     }
