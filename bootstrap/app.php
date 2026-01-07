17f2f85eae2123ff53764fc173e0e02f7e37801a
Reftec IT <reftec.it@gmail.com>
2026-01-07 17:27:19 +0800

    feat: configure middleware to include LogoutIfArchived for user session management

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\UniqueVisitorTracking;
+use App\Http\Middleware\LogoutIfArchived;
 
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web([
             UniqueVisitorTracking::class,
         ]);
+        $middleware->appendToGroup('web', LogoutIfArchived::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
