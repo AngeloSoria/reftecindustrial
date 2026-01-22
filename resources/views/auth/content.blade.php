6fd91d93685cfba5c2a1faa8c5c6a096faa2e82a
Reftec IT <reftec.it@gmail.com>
2026-01-22 10:55:17 +0800

    feat: implement dynamic content loading in General component

--- resources/views/auth/content.blade.php
+++ resources/views/auth/content.blade.php
@@ -1,15 +1,20 @@
 <x-layouts.auth.app viewName="Content" class="font-inter px-2">
-    <div class="bg-white shadow-md rounded-xl custom-min-h my-2 mx-auto py-6 px-4 sm:px-6 lg:px-8">
+    <div x-data="handleGeneral()"
+        class="bg-white shadow-md rounded-xl custom-min-h my-2 mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <!-- Tabs Navigation -->
         <div class="border-b border-gray-200">
-            <nav class="-mb-px justify-center-safe sm:justify-start flex flex-wrap space-x-8 [&>*]:cursor-pointer" aria-label="Tabs">
-                <button id="general-tab" class="tab-button whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm" onclick="openTab('general')">
+            <nav class="-mb-px justify-center-safe sm:justify-start flex flex-wrap space-x-8 [&>*]:cursor-pointer"
+                aria-label="Tabs">
+                <button id="general-tab" class="tab-button whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm"
+                    onclick="openTab('general')">
                     General
                 </button>
-                <button id="projects-tab" class="tab-button whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm" onclick="openTab('projects')">
+                <button id="projects-tab" class="tab-button whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm"
+                    onclick="openTab('projects')">
                     Projects
                 </button>
-                <button id="products-tab" class="tab-button whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm" onclick="openTab('products')">
+                <button id="products-tab" class="tab-button whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm"
+                    onclick="openTab('products')">
                     Products
                 </button>
             </nav>
@@ -29,7 +34,48 @@
             <x-layouts.auth.content.products />
         </div>
     </div>
+    <script>
+        function handleGeneral() {
+            return {
+                heroData: null,
+                productLinesData: null,
+                historyData: null,
+                aboutUsData: null,
+                generalContentsLoaded: false,
+                async init() {
+                    const response = await fetch('{{ route('content.get.section.all') }}');
+                    const data = await response.json();
+                    // console.log(data);
 
+                    // hero
+                    if (!data.hero && !data.hero.original) {
+                        console.error("Failed to load hero data");
+                    }
+                    this.heroData = data.hero.original;
+
+                    // product_lines
+                    if (!data.product_lines && !data.product_lines.original) {
+                        console.error("Failed to load hero data");
+                    }
+                    this.productLinesData = data.product_lines.original; // yeah... cache inside cache
+
+                    // history
+                    if (!data.history && !data.history.original) {
+                        console.error("Failed to load hero data");
+                    }
+                    this.historyData = data.history.original;
+
+                    // About us
+                    if (!data.about_us && !data.about_us.original) {
+                        console.error("Failed to load hero data");
+                    }
+                    this.aboutUsData = data.about_us.original.original; // yeah... cache inside cache
+
+                    this.generalContentsLoaded = true;
+                }
+            };
+        }
+    </script>
     <script>
         function openTab(tabName) {
             // Hide all tab contents
@@ -53,12 +99,10 @@
         }
 
         // Initialize the first tab as active
-        document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', function () {
             openTab("{{ session('content') ? session('content')['tab'] : 'general' }}");
         });
     </script>
 
     <x-public.modal.image_preview escKeyToClose clickOutsideToClose />
 </x-layouts.auth.app>
-
-
