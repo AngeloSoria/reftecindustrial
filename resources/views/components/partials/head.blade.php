263cab7c4bd09a6dee04d1cec825c918f2deb3e0
Reftec IT <reftec.it@gmail.com>
2026-01-27 16:21:44 +0800

    fix: add meta tags for improved SEO and accessibility

--- resources/views/components/partials/head.blade.php
+++ resources/views/components/partials/head.blade.php
@@ -2,6 +2,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="title" content="Reftec Industrial Supply & Services Inc.">
+    <meta name="description"
+        content="The main business is installing ice plants and cold storages and fabricating ice plant components. It also supplies and installs commercial water tanks and other services related to water industry and …">
+    <meta name="keywords"
+        content="refrigeration, construction, industrial, supply, reftec, reftec industrial, water system, cold storage, water tanks">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+
+
     {{-- FavIcon --}}
     <link rel="shortcut icon" href="{{ asset('images/logo_favicon_64x64_3.png') }}" type="image/x-icon">
 
@@ -21,7 +31,7 @@
             @foreach(session('toasts') as $toast)
                 toast(@json($toast['message']), @json($toast['type']), @json($toast['duration']));
             @endforeach
-        });
+            });
     </script>
 @endif
 
